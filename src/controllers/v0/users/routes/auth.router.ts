4e3ed27aa0b6bb1b714e8d1676735cf5a1e160c3
Ben Ethington <benaminde@gmail.com>
2021-06-01 00:17:38 -0500

    build for windows script

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -109,10 +109,9 @@ router.post('/', async (req: Request, res: Response) => {
 
     const password_hash = await generatePassword(plainTextPassword);
 
-    const newUser = await new User({
-        email: email,
-        password_hash: password_hash
-    });
+    const newUser = new User();
+    newUser.email = email;
+    newUser.password_hash = password_hash;
 
     let savedUser;
     try {
